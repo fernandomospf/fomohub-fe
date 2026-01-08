39e2fdae78f5b4772d922ada2ba2bce242da60b3
Fernando Mos <fernandomosp@gmail.com>
2026-01-08 16:38:12 -0300

    chore: remove .next build artifacts from repo

--- .next/dev/static/chunks/src_App_tsx_bf11300b._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_0e7a93e3._.js",
-  "static/chunks/node_modules_@remix-run_router_dist_router_952b9678.js",
-  "static/chunks/node_modules_4399bac1._.js"
-],
-    source: "dynamic"
-});
